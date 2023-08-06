a7d09d9d18e75b107de8a81591d7aeb64a5d24fe
chezard <subhanmasimov02@gmail.com>
2023-08-06 15:33:42 +0400

    initial

---
+++ src/main/java/com/threesteps/acpapi/AcpApiApplication.java
@@ -0,0 +1,21 @@
+package com.threesteps.acpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class AcpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcpApiApplication.class, args);
+	}
+
+	@GetMapping
+	public String ok(){
+		return "Ok";
+	}
+
+}
---
+++ src/test/java/com/threesteps/acpapi/AcpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threesteps.acpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
