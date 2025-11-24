eef1e93b348888fc885dc4db7ea58f2555efca6f
FeridSadiqzada <feridsadiqzade@gmail.com>
2025-11-24 15:47:02 +0400

    Create CreateServiceItemRequest.kt

---
+++ src/main/java/com/threesteps/acpapi/dto/CreateServiceItemRequest.kt
@@ -0,0 +1,19 @@
+package com.threesteps.acpapi.dto
+
+import java.time.LocalDateTime
+
+data class CreateServiceItemRequest @JvmOverloads constructor(
+
+    var titleAz: String? = null,
+    var titleEn: String? = null,
+    var titleRu: String? = null,
+    var descriptionAz: String? = null,
+    var descriptionEn: String? = null,
+    var descriptionRu: String? = null,
+    var iconUrl: String? = null,
+    var orderNumber: Int? = null,
+    var createDate: LocalDateTime? = null,
+    var status: Boolean? = null
+
+) {
+}
