dde15192aef5f09af5827a1ad02b5463e09cb1a3
FeridSadiqzada <feridsadiqzade@gmail.com>
2025-11-29 23:28:39 +0400

    Update ServiceDescriptionMapper.java

--- src/main/java/com/threesteps/acpapi/mapper/ServiceDescriptionMapper.java
+++ src/main/java/com/threesteps/acpapi/mapper/ServiceDescriptionMapper.java
@@ -2,13 +2,31 @@ package com.threesteps.acpapi.mapper;
 
 import com.threesteps.acpapi.dto.ServiceDescriptionDto;
 import com.threesteps.acpapi.model.ServiceDescription;
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface ServiceDescriptionMapper {
+@Component
+public class ServiceDescriptionMapper {
 
-    ServiceDescriptionDto toDto(ServiceDescription serviceDescription);
+    public ServiceDescriptionDto toDto(ServiceDescription from) {
+        if (from == null) return null;
 
-    ServiceDescription toEntity(ServiceDescriptionDto serviceDescriptionDto);
+        return new ServiceDescriptionDto(
+                from.getId(),
+                from.getDescriptionAz(),
+                from.getDescriptionEn(),
+                from.getDescriptionRu()
+        );
+    }
+
+    public ServiceDescription toEntity(ServiceDescriptionDto from) {
+        if (from == null) return null;
+
+        return new ServiceDescription(
+                from.getId(),
+                from.getDescriptionAz(),
+                from.getDescriptionEn(),
+                from.getDescriptionRu()
+        );
+    }
 
 }
